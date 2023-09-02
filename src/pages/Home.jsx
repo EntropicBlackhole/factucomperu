5b789374666260c3ac6a40699cc7ff588b25e00e
EntropicBlackhole <82614490+EntropicBlackhole@users.noreply.github.com>
2023-09-01 22:41:20 -0500

    Añadi la opcion de un demo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,19 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 function Home() {
-    return (
-			<div className="home-wrapper">
-				<img src="../../fcp.svg" className="login-logo"></img>
-				<h2>Home page</h2>
-				<Link to="/login">
-					<button>Login</button>
-				</Link>
-				<Link to="/register">
-					<button>Register</button>
-				</Link>
-			</div>
-		);
+	return (
+		<div className="home-wrapper">
+			<img src="../../fcp.svg" className="login-logo"></img>
+			<h2>Home page</h2>
+			<Link to="/login">
+				<button>Login</button>
+			</Link>
+			<Link to="/register">
+				<button>Register</button>
+			</Link>
+			<Link to="/dashboard">Demo</Link>
+		</div>
+	);
 }
 
-export default Home
+export default Home;
